d288600f3065ed75411bd9a25c633fe738f1c3b5
Marat <razorlevel@gmail.com>
2020-10-19 21:35:21 +0300

    change packege.json file

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {render} from 'react-dom'
 
 const App = () => {
-    return <h1>Hello</h1>
+    return <h1>Hello</h1> 
 }
 
 render(<App/>, document.getElementById('app'))
